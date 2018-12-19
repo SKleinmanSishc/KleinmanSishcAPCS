45cca4e66af08f7a1804c889c5ca78d4aa70995d
Sean Kleinman Sishc <Klein.Sean2021@ssusd.org>
2018-12-19 07:28:16 -0800

    adds functions

---
+++ Assignments/Functions.java
@@ -0,0 +1,24 @@
+class Functions {
+    public static int testNum1 = 6;
+    public static int  testNum2 = 13;
+    
+
+    public static void main(String[] args){
+        boolean Result = bothEven(testNum1, testNum2);
+        if(Result == true){
+            evenPrint(testNum1, testNum2);
+        }
+        else{
+            oddPrint(testNum1, testNum2);
+        }
+    }
+    public static boolean bothEven(int x, int i){
+       return ((x % 2 == 0) && (i % 2 == 0));
+    }
+    public static void evenPrint(int x, int i){
+        System.out.println(x + " and " + i + " are even");
+    }
+     public static void oddPrint(int x, int i){
+        System.out.println(x + " and " + i + " are not even");
+    }
+}
